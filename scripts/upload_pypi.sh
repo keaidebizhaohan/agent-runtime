5d16bb36f2c19c4dd79422a7b28f80acffa2ab6f
chen-hui-zhe-xi <chenhui280@h-partners.com>
2026-05-09 16:32:48 +0800

    fix version error
    Co-authored-by: chen-hui-zhe-xi<chenhui280@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !230 merge fix-version into develop
    
    fix version error
    
    Created-by: chen-hui-zhe-xi
    Commit-by: chen-hui-zhe-xi
    Merged-by: ZYQ5333
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind bug
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [x] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [x] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [x] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [x] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [x] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    See merge request: openJiuwen/agent-runtime!230

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -18,6 +18,7 @@ DEFAULT_PKG_DIRS=(
     service
     server
     applications/lowcode_agent
+    applications/ir_execution_service
 )
 
 # 如果传入参数，则使用传入的目录
@@ -37,18 +38,18 @@ do
     # 修改内部依赖版本号
     case "${pkg_dir}" in
         management|service)
-            sed -i 's/"openjiuwen-runtime-foundation==.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-foundation.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
             ;;
         cli|server)
-            sed -i 's/"openjiuwen-runtime-foundation==.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
-            sed -i 's/"openjiuwen-runtime-management==.*"/"openjiuwen-runtime-management=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-foundation.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-management.*"/"openjiuwen-runtime-management=='"${VERSION}"'"/' pyproject.toml
             ;;
         applications/lowcode_agent)
-            sed -i 's/"openjiuwen-runtime-service==.*"/"openjiuwen-runtime-service=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-service.*"/"openjiuwen-runtime-service=='"${VERSION}"'"/' pyproject.toml
             ;;
         applications/ir_execution_service)
-            sed -i 's/"openjiuwen-runtime-foundation==.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
-            sed -i 's/"openjiuwen-runtime-service==.*"/"openjiuwen-runtime-service=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-foundation.*"/"openjiuwen-runtime-foundation=='"${VERSION}"'"/' pyproject.toml
+            sed -i 's/"openjiuwen-runtime-service.*"/"openjiuwen-runtime-service=='"${VERSION}"'"/' pyproject.toml
             ;;
         *)
             ;;
